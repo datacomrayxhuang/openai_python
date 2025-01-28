ab7db29017515ab4fc7417194a2153b0990db053
Ray <ray.huang@datacom.co.nz>
2025-01-28 13:03:59 +1300

    first commit

---
+++ main.py
@@ -0,0 +1,42 @@
+'''
+from openai import OpenAI
+
+client = OpenAI(
+    base_url = '',
+    api_key = ''
+)
+
+print('\nSystem: Welcome! Let\'s start chatting! You may end a conversation by inputting \'End\'.')
+
+conversation_history = [
+    {"role": "system", "content": "You are a helpful assistant."},
+]
+
+while True:
+    end_conversation = False
+    prompt = input('User: ')
+    conversation_history.append(
+        {"role": "user", "content": prompt}
+    )
+
+    if prompt.lower() == 'end':
+        end_conversation = True
+    
+    try:
+        chat_completion = client.chat.completions.create(
+            messages=conversation_history,
+            model="Gpt4o",
+        )
+        
+        response = chat_completion.choices[0].message.content
+        print(f"Assistant: {response}")
+        conversation_history.append(
+            {"role": "assistant", "content": response}
+        )
+
+        if end_conversation:
+            break
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        break
+'''
