29d8b8e4a990619ed1aeb5b38699125df961eda2
Ray <ray.huang@datacom.co.nz>
2025-02-05 13:41:17 +1300

    Minor updates

--- task1.py
+++ task1.py
@@ -6,6 +6,7 @@ from openai import OpenAI
 openai_base_url = os.environ.get("OPENAI_BASE_URL")
 openai_api_key = os.environ.get("OPENAI_API_KEY")
 
+# Basic UI set up
 st.title("💬 Chatbot")
 st.caption("🚀 A Streamlit chatbot powered by OpenAI")
 if "messages" not in st.session_state:
@@ -14,24 +15,29 @@ if "messages" not in st.session_state:
 for message in st.session_state.messages:
     st.chat_message(message["role"]).write(message["content"])
 
+# OpenAI client
 client = OpenAI(
     base_url = openai_base_url,
     api_key = openai_api_key,
 )
 
 if prompt := st.chat_input():
-    # Record user input
-    st.session_state.messages.append({"role": "user", "content": prompt})
-    # Display the input on UI
-    st.chat_message("user").write(prompt)
-    # Call OpenAI API with history
-    response = client.chat.completions.create(
-        model = "Gpt4o",
-        messages = st.session_state.messages,
-    )
-    # Get response
-    result = response.choices[0].message.content
-    # Record response
-    st.session_state.messages.append({"role": "assistant", "content": result})
-    # Write response to history
-    st.chat_message("assistant").write(result)
+    try:
+        # Record user input
+        st.session_state.messages.append({"role": "user", "content": prompt})
+        # Display the input on UI
+        st.chat_message("user").write(prompt)
+        # Call OpenAI API with history
+        response = client.chat.completions.create(
+            model = "Gpt4o",
+            messages = st.session_state.messages,
+        )
+        # Get response
+        result = response.choices[0].message.content
+        # Record response
+        st.session_state.messages.append({"role": "assistant", "content": result})
+        # Write response to history
+        st.chat_message("assistant").write(result)
+    except Exception as e:
+        st.error(f"Error: {e}")
+        st.stop()
--- task3.py
+++ task3.py
@@ -5,7 +5,9 @@ import chromadb
 from pydantic import BaseModel, Field
 from openai import OpenAI
 from openai.types.chat.chat_completion import ChatCompletionMessage
+from openai.types.chat.chat_completion_message_tool_call import ChatCompletionMessageToolCall
 
+# Persistent database
 chroma_client = chromadb.PersistentClient(path = "./db/task3/chroma")
 collection = chroma_client.get_collection(name = 'task3')
 
@@ -13,21 +15,21 @@ collection = chroma_client.get_collection(name = 'task3')
 openai_base_url = os.environ.get("OPENAI_BASE_URL")
 openai_api_key = os.environ.get("OPENAI_API_KEY")
 
+# Basic UI set up
 st.title("📝 Trip planner with OpenAI")
 if "messages" not in st.session_state:
     st.session_state["messages"] = [
         {"role": "system", "content": "You are an automated trip planning assistant"},
         {"role": "assistant", "content": "Ask me to plan a trip for you."},
     ]
-if "disabled" not in st.session_state:
-    st.session_state.disabled = False
 
-for msg in st.session_state.messages:
-    if type(msg) == ChatCompletionMessage:
-        msg = msg.model_dump()
-    if msg["role"] != "system" and msg["role"] != "tool" and msg["content"] != None:
-        st.chat_message(msg["role"]).write(msg["content"])
+for message in st.session_state.messages:
+    if type(message) == ChatCompletionMessage:
+        message = message.model_dump()
+    if message["role"] != "system" and message["role"] != "tool" and message["content"] != None:
+        st.chat_message(message["role"]).write(message["content"])
 
+# OpenAI client
 client = OpenAI(
     base_url = openai_base_url,
     api_key = openai_api_key,
@@ -41,28 +43,26 @@ locally_stored_documents = [
     "tauranga_bay_of_plenty_attraction.pdf",
 ]
 
+# Structured output for first LLM call: determine if the input is a trip planning event
 class EventExtraction(BaseModel):
-    """First LLM call: Extract basic event information"""
     description: str = Field(description = "Raw description of the event")
-    is_trip_planning_event: bool = Field(
-        description = "Whether this text describes a trip planning event"
-    )
-    should_query_local_data: bool = Field(
-        description = f"Whether this event involves travelling to one of the cities that has local data: {locally_stored_documents}"
-    )
+    is_trip_planning_event: bool = Field(description = "Whether this text describes a trip planning event")
+    location: list[str] = Field(description = "List of location of the event")
+    should_query_local_data_for_location: list[bool] = Field(description = f"Given local data: {locally_stored_documents}, in the same order of list of location of the event, whether query local data for a location")
     trip_duration: str = Field(description = "Duration of the trip, if unknown, answer unknown")
-    location: str = Field(description = "Location of the event")
     confidence_score: float = Field(description = "Confidence score between 0 and 1")
 
+# Structured output for second LLM call
 class Attraction(BaseModel):
     name: str = Field(description = "Name of the attraction")
     description: str = Field(description = "Description of the attraction")
     duration: str = Field(description = "Duration of the attraction")
-    # confidence_score: float = Field(description = "Confidence score between 0 and 1")
 
+# Structured output for second LLM call
 class AttractionExtraction(BaseModel):
     attractions: list[Attraction] = Field(description = "List of attractions in this location")
 
+# Tools available to LLM
 tools = [
     {
         "type": "function",
@@ -145,15 +145,12 @@ tools = [
     },
 ]
 
-# First LLM call to determine if input is a trip planning event
+# First tool LLM can call to determine if input is a trip planning event
 def analyse_input(user_input: str) -> EventExtraction:
     completion = client.beta.chat.completions.parse(
         model = "Gpt4o",
         messages = [
-            {
-                "role": "system",
-                "content": f"Analyze if the text describes a trip planning event.",
-            },
+            {"role": "system", "content": f"Analyse if the text describes a trip planning event."},
             {"role": "user", "content": user_input},
         ],
         response_format = EventExtraction,
@@ -161,7 +158,7 @@ def analyse_input(user_input: str) -> EventExtraction:
     result = completion.choices[0].message.parsed
     return result
 
-# Second LLM call to get attractions for a location (may or may not use local data)
+# Second tool LLM can call to get attractions for a location (may or may not use local data)
 def get_attractions(location: str, should_query_local_data: bool) -> AttractionExtraction:
     message = ''
     if should_query_local_data:
@@ -169,50 +166,37 @@ def get_attractions(location: str, should_query_local_data: bool) -> AttractionE
         message = f"Get attractions in this location: {location}, prioritise using the following local data: {local_data}."
     else:
         message = f"Get attractions in this location: {location}."
+    
     completion = client.beta.chat.completions.parse(
         model = "Gpt4o",
-        messages = [
-            {
-                "role": "system",
-                "content": message,
-            },
-        ],
+        messages = [{"role": "system", "content": message}],
         response_format = AttractionExtraction,
     )
     result = completion.choices[0].message.parsed
     return result
 
-# Third LLM call to generate itinerary
+# Third tool LLM can call to generate itinerary
 def generate_itinerary(attractions: list[Attraction], duration: str) -> str:
     message = ''
     if duration != 'unknown':
         message = f"Generate an itinerary for the following attractions: {attractions}, spread into {duration}."
     else:
         message = f"Generate an itinerary for the following attractions: {attractions}, spread into suitable days."
+    
     completion = client.chat.completions.create(
         model = "Gpt4o",
-        messages = [
-            {
-                "role": "system",
-                "content": message,
-            },
-        ],
+        messages = [{"role": "system", "content": message}],
     )
     result = completion.choices[0].message.content
     return result
 
-# Forth LLM call to generate itinerary
+# Forth tool LLM can call to generate itinerary
 def get_trip_summary(user_input: str, itinerary: str) -> str:
     message = f"Using the itinerary: {itinerary}.\n\nSummarise a natural language response to the user's goal: {user_input}."
-    # st.chat_message("system").write(message)
+
     completion = client.chat.completions.create(
         model = "Gpt4o",
-        messages = [
-            {
-                "role": "system",
-                "content": message,
-            },
-        ],
+        messages = [{"role": "system", "content": message}],
     )
     result = completion.choices[0].message.content
     return result
@@ -229,6 +213,7 @@ def query_local_data(location: str) -> str:
         local_data += f"""{chunk}\n\n"""
     return local_data
 
+# Helper function to make tool call
 def call_function(name, args):
     if name == "analyse_input":
         return analyse_input(**args)
@@ -239,15 +224,25 @@ def call_function(name, args):
     elif name == "get_trip_summary":
         return get_trip_summary(**args)
 
-def make_tool_calls(completion):
+def make_tool_calls(tool_calls: list[ChatCompletionMessageToolCall]):
     st.session_state.messages.append(completion.choices[0].message)
-    for tool_call in completion.choices[0].message.tool_calls:
+    step_messages = []
+
+    for tool_call in tool_calls:
         name = tool_call.function.name
         args = json.loads(tool_call.function.arguments)
+
+        step_message = ''
         if name == 'get_attractions' and args['should_query_local_data']:
-            st.chat_message("assistant").write(f"Step: {name} for {args['location']} (use local data)")
+            step_message = f"Step: {name} for {args['location']} (use local data)"
+        elif name == 'get_attractions' and not args['should_query_local_data']:
+            step_message = f"Step: {name} for {args['location']}"
         else:
-            st.chat_message("assistant").write(f"Step: {name}")
+            step_message = f"Step: {name}"
+
+        step_messages.append(step_message)
+        st.chat_message("assistant").write(step_message)
+        # st.session_state.messages.append({"role": "assistant", "content": step_message})
 
         function_result = call_function(name, args)
         # st.chat_message("system").write(function_result)
@@ -259,19 +254,14 @@ def make_tool_calls(completion):
         
         st.session_state.messages.append({"role": "tool", "tool_call_id": tool_call.id, "content": json.dumps(function_result)})
 
-def enable():
-    st.session_state.disabled = False
+    for step_message in step_messages:
+        st.session_state.messages.append({"role": "assistant", "content": step_message})
 
-def disable():
-    st.session_state.disabled = True
-
-prompt = st.chat_input(disabled = st.session_state.disabled)
-
-if prompt != None and prompt != '':
+if prompt := st.chat_input():
     st.session_state.messages.append({"role": "user", "content": prompt})
     st.chat_message("user").write(prompt)
 
-    sys_message = f"User wants to achieve the goal of {prompt}. Start with calling analyse_input, break it down into steps, in each step, only use tools provided."
+    sys_message = f"User wants to achieve the goal of {prompt}. Start with calling analyse_input if you haven't, break it down into steps, in each step, only use tools provided."
     st.session_state.messages.append({"role": "system", "content": sys_message})
     completion = client.chat.completions.create(
         model = "Gpt4o",
@@ -280,11 +270,11 @@ if prompt != None and prompt != '':
     )
     result = completion.choices[0].message.content
 
-    max_steps = 10
+    max_steps = 20
     step = 1
     
     while result == None and step <= max_steps:
-        st.session_state.messages.append({"role": "system", "content": "What is the next step or next tool to call?"})
+        # st.session_state.messages.append({"role": "system", "content": "What is the next step or next tool to call?"})
         # st.chat_message("system").write(st.session_state.messages)
         completion = client.chat.completions.create(
             model = "Gpt4o",
@@ -295,8 +285,7 @@ if prompt != None and prompt != '':
         result = completion.choices[0].message.content
         if completion.choices[0].message.tool_calls != None:
             # st.chat_message("system").write(completion.choices[0].message.tool_calls)
-            make_tool_calls(completion)
+            make_tool_calls(completion.choices[0].message.tool_calls)
     
     st.session_state.messages.append({"role": "assistant", "content": result})
     st.chat_message("assistant").write(result)
-    enable()
